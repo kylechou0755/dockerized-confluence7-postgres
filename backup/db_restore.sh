0cac72fbcacc3ba5800f6fefaaa8ed99d874cf9f
Eduard Saryan <eduardevops@users.noreply.github.com>
2020-05-01 00:23:04 +0400

    Update db_restore.sh

--- backup/db_restore.sh
+++ backup/db_restore.sh
@@ -3,4 +3,4 @@
 source /opt/dockerized-jira8-mysql/.env.db
 
 # Restore Stackpost DB
-docker exec  jira-db psql -U $POSTGRES_USER -d $POSTGRES_DB < /path/to/bkup-file/pgdump.sql
+docker exec  jira-db psql -U $POSTGRES_USER -d $POSTGRES_DB < /path/to/backup-file/pgdump.sql
